5283578dc744ebbd20d49b2555667bd4ee8729b3
Chris Crowell <crowelch@mail.uc.edu>
2015-08-27 19:18:30 -0400

    save to firebase

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
+var firebase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,12 @@ router.post('/helloworld', function(req, res, next) {
 
 	res.type('text/xml');
 	res.send(resp.toString());
+
+	var myDb = new firebase('https://bearcathackers-twilio.firebaseio.com');
+	myDb.set({
+		phone: req.body.From,
+		response: req.body.Body
+	});
 });
 
 module.exports = router;
